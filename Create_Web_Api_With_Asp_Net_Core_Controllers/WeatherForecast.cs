5b5d40a8e22951ed0fdaad5affb33a8e89af9339
EnriLion <enriqueleon0201leon@gmail.com>
2024-06-27 23:43:37 -0600

    Adding Proyect Web APPI ASP.NET CORE

---
+++ Create_Web_Api_With_Asp_Net_Core_Controllers/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace Create_Web_Api_With_Asp_Net_Core_Controllers;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
