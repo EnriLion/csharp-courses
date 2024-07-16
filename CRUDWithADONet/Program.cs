0d4572b220a60747c613ad073aedac87c36d69f3
EnriLion <enriqueleon0201leon@gmail.com>
2024-07-15 22:53:59 -0600

    Updating files

--- CRUDWithADONet/Program.cs
+++ CRUDWithADONet/Program.cs
@@ -1,7 +1,10 @@
+using CRUDWithADONet.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<Employee_DAL>();
 
 var app = builder.Build();
 
--- CRUDWithADONet/obj/Debug/net7.0/CRUDWithADONet.AssemblyInfo.cs
+++ CRUDWithADONet/obj/Debug/net7.0/CRUDWithADONet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUDWithADONet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39f868c7fa7fa180bdf55a3413e5b7dc8ef0cfda")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd2b8c4511ecce0d99be7d625ce80d88d9dbc42c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUDWithADONet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUDWithADONet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
